0ea3d5acc589d66a05b645f6837a88479d5052e9
Izzyflowz <nnadozieezekiel@gmail.com>
2023-08-12 02:52:17 -0700

    TASK 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	c = 'A';
+	while
+		(c <= 'z')  {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z')  {
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
