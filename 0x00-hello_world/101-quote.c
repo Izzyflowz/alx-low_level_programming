5b23a52918ddec61c3e57052f6c2957f87348650
Izzyflowz <nnadozieezekiel@gmail.com>
2023-08-14 19:20:35 -0700

    TASK 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,9 @@
  * main - A program that prints exactly to the standard error
  * Return: 1
  */
+
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
